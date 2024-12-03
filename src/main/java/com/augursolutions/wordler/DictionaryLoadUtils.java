c532f5fdb3f341dbf2037c38c0eb2f0a74c1efda
AUGURSOLUTIONS0\stmajo <steven.major@augursolutions.com>
2024-12-03 16:09:50 -0500

    * Switched from ant to Maven  * moved all tests into test class (use org.reflection to test all Dictionary classes)

---
+++ src/main/java/com/augursolutions/wordler/DictionaryLoadUtils.java
@@ -0,0 +1,131 @@
+package com.augursolutions.wordler;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.augursolutions.wordler.Word.Part_Of_Speech;
+
+/**
+ * Utility class with methods for populating a {@link TreeMapLanguageDictionary} object from external sources
+ * 
+ * @author Steven Major
+ *
+ */
+public final class DictionaryLoadUtils {
+
+	// private constructor - all methods are static
+	private DictionaryLoadUtils() {};
+	
+	/**
+	 * Populate a {@link TreeMapLanguageDictionary} by reading in a text file where each line
+	 * contains a word followed by a definition. Intended for use with the 
+	 * text file dictionaries in the <b><i>data/words/&lt;region&gt;</i></b> folder
+	 * of a <b><i>Zyzzyva</i></b> installation. Any text file with one word per line
+	 * can be loaded with this method. Definitions do not need to be present
+	 * @param dictionaryFile {@link Path} to the text file with words
+	 *        and definitions.
+	 * @return A dictionary object with all words  from the specified text file
+	 */
+	public static final <T extends Dictionary> boolean loadFromZyzzyva(T dictionary, Path dictionaryFile) {	
+		File wordList = null;
+		try {
+			wordList = dictionaryFile.toFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		if(wordList == null || !wordList.exists()) {
+			System.out.println("File '" + dictionaryFile.toString() + "' does not exist or is not readable.");
+			return false;
+		}
+		try(BufferedReader br = new BufferedReader(new FileReader(wordList)); ) {
+			String line = null;
+			while ((line = br.readLine()) != null)  {  
+				String definitionEntry = null;
+				List<String> definitions = new ArrayList<>();
+				List<Part_Of_Speech> partsOfSpeech = new ArrayList<>();
+				// Get the word and definition portions of the entry
+				int spaceIndex = line.indexOf(' ');
+				// If there is no space then this is just a single word entry, otherwise it is "word the definition of the word"
+				// in which case we want to pull apart "word" and "the definition of the word"
+				String word = spaceIndex == -1 ? line : line.substring(0, line.indexOf(' '));
+				if(spaceIndex > -1 && line.length() > spaceIndex)
+					definitionEntry = line.substring(spaceIndex+1);
+				// For TreeMapLanguageDictionary, add a Word objectw ith definitions and parts of speech
+				if(dictionary instanceof TreeMapLanguageDictionary) {
+					if(definitionEntry != null && !definitionEntry.isBlank()) {
+						// Definitions have the following format:
+						// - homographs (same spelling, different word) are separated by " / "
+						// - part of speech occurs at the end of the definition and appears in square brackets, followed by conjugations/pluralization
+						// - related words follow the part of speech and are preceded by " : ", e.g. the word "WORK" (verb) might have " : WORKER [n]"
+						// e.g. DIM {obscure=adj} [adj DIMMER, DIMMEST] : DIMLY [adv], DIMMISH [adj], DIMNESS [n] / to make dim [v DIMMED, DIMMING, DIMS] : DIMMABLE [adj]
+						String[] allDefs = definitionEntry.split(" / ");
+						for(String def : allDefs) {
+							definitions.add(def);
+							partsOfSpeech.add(getPartOfSpeechFromZyzzyva(def));
+						}
+
+					}
+					((TreeMapLanguageDictionary)dictionary).add(new Word(word,definitions,partsOfSpeech));
+				} else {
+					dictionary.add(word.toUpperCase());
+				}
+			} 
+		} 
+		catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Get the part of speech from a single definition - looks for the first entry in square brackets
+	 * @param definition A word definition in the Zyzzyva format
+	 * @return {@link Part_Of_Speech}
+	 */
+	private static final Part_Of_Speech getPartOfSpeechFromZyzzyva(String definition) {
+		if(definition == null || definition.isBlank())
+			return Part_Of_Speech.UNDEFINED;
+		
+		// Look for any related word entries and strip them off
+		int relatedWordIndex = definition.indexOf(" : ");
+		if(relatedWordIndex > -1) {
+			definition = definition.substring(0, relatedWordIndex);
+		}
+		Pattern pattern = Pattern.compile("\\[([a-z]+)\\s?[\\w\\s,]*\\]");
+		Matcher matcher = pattern.matcher(definition);
+		if(matcher.find()) {
+			String partOfSpeech = matcher.group(1);
+			switch(partOfSpeech) {
+			case "adj":
+				return Part_Of_Speech.ADJECTIVE;
+			case "adv":
+				return Part_Of_Speech.ADVERB;
+			case "article":
+				return Part_Of_Speech.ARTICLE;
+			case "conj":
+				return Part_Of_Speech.CONJUNCTION;
+			case "interj":
+				return Part_Of_Speech.INTERJECTION;
+			case "n":
+				return Part_Of_Speech.NOUN;
+			case "prep":
+				return Part_Of_Speech.PREPOSITION;
+			case "pron":
+				return Part_Of_Speech.PRONOUN;
+			case "v" :
+				return Part_Of_Speech.VERB;
+			default:
+				return Part_Of_Speech.UNDEFINED;
+			}
+		}
+		return Part_Of_Speech.UNDEFINED;
+	}
+}
---
+++ src/main/java/com/augursolutions/wordler/SpellingBeeSolver.java
@@ -0,0 +1,395 @@
+package com.augursolutions.wordler;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Class designed to streamline solving the daily <b>NYT Spelling Bee</b> puzzle. Example usage:
+ * <pre>
+ *		SpellingBeeSolver slvr = new SpellingBeeSolver();
+ *		slvr.setDictionary(dictionary);
+ *		slvr.setPossibleLetters("PHANTO");
+ *		slvr.setRequiredLetter("M");
+ *		SpellingBeeSolution sln = slvr.solve();
+ *		sln.prettyPrint();
+ * </pre>
+ * The {@link SpellingBeeSolution#prettyPrint()} method will display all answers in the console, e.g.
+ * <pre>
+ *PANGRAM(S):
+ *	PHANTOM
+ *
+ *WORDS OF LENGTH 9:
+ *	MANHATTAN
+ *WORDS OF LENGTH 8:
+ *	PHOTOMAP
+ *WORDS OF LENGTH 7:
+ *	MAHATMA
+ *  ...
+ * </pre>
+ * <p>
+ * <b>NOTE:</b> The solutions are correct to the extent that the {@link TreeMapLanguageDictionary} object
+ * used to generate them matches the dictionary used by the <b>NYT Spelling Bee</b>
+ * 
+ * 
+ * @author Steven Major
+ *
+ */
+public class SpellingBeeSolver {
+	private static final int N_POSSIBLE_LETTERS = 6;
+	private String requiredLetter;
+	private String possibleLetters;
+	private TreeMapLanguageDictionary dictionary;
+	
+	public SpellingBeeSolver() {};
+	
+	/**
+	 * Assigns the {@link TreeMapLanguageDictionary} that will be used to generate all solutions
+	 * @param d A {@link TreeMapLanguageDictionary} with all words to get solutions from ({@code allAnswers} will be a subset of this dictionary)
+	 */
+	public void setDictionary(TreeMapLanguageDictionary d) {
+	  this.dictionary = d;	
+	}
+	
+	/**
+	 * Get the associated {@link TreeMapLanguageDictionary} object - initializes an empty {@link TreeMapLanguageDictionary} if one has not been set.
+	 * @return The {@link TreeMapLanguageDictionary} object associated with the solver
+	 */
+	public TreeMapLanguageDictionary getDictionary() {
+		if(this.dictionary == null) {
+			this.dictionary = new TreeMapLanguageDictionary();
+		}
+		return this.dictionary;
+	}
+	
+	/**
+	 * Determine if a non-empty {@link TreeMapLanguageDictionary} has been set
+	 * @return {@code true} if the {@link TreeMapLanguageDictionary} object is not null and not empty
+	 */
+	public boolean hasValidDictionary() {
+		return (this.getDictionary() != null && this.getDictionary().getSize() > 0);
+	}
+	
+	/**
+	 * Set the "required" letter in the puzzle (yellow letter in the center). String
+	 * should be a single letter (A-Z), e.g. {@code setRequiredLetter("W");}
+	 * @param r A single letter (A-Z)
+	 */
+	public void setRequiredLetter(String r) {
+		if(r == null || r.isEmpty() || r.length() != 1) {
+			System.out.println("ERROR: setRequiredLetter() takes a String with one character, value provided was: " + r);
+			return;
+		}
+		r = r.toUpperCase();
+		Character c = r.charAt(0);
+		if(c<'A' || c>'Z') {
+			System.out.println("ERROR: setRequiredLetter() takes a String with a letter (A-Z), value provided was: " + r);
+			return;
+		}
+		this.requiredLetter=r;
+	}
+	
+	/**
+	 * Get the {@code requiredLetter} field
+	 * @return The {@code requiredLetter} field
+	 */
+	public String getRequiredLetter( ) {
+		return this.requiredLetter;
+	}
+	
+	/**
+	 * Determine if the value of {@code requiredLetter} is valid:
+	 *  - Not null
+	 *  - Contains exactly one letter
+	 *  - Letter is in A-Z
+	 * @return {@code true} if {@code requiredLetter} is valid.
+	 */
+	public boolean hasValidRequiredLetter() {
+		return (this.requiredLetter != null && 
+				this.requiredLetter.length() == 1 &&
+				this.requiredLetter.toUpperCase().charAt(0) >= 'A' && 
+				this.requiredLetter.toUpperCase().charAt(0) <= 'Z');
+	}
+	
+	/**
+	 * @param p A {@link String} with 6 unique characters - performs validation on the input.
+	 */
+	public void setPossibleLetters(String p) {
+		if(p == null || p.isEmpty() || p.length() != 6) {
+			System.out.println("ERROR: setPossibleLetters() takes a String with " + N_POSSIBLE_LETTERS + " characters, value provided was: " + p);
+			return;
+		}
+		p = p.toUpperCase();
+		for(int i=0; i<N_POSSIBLE_LETTERS; i++) {
+			Character c = p.charAt(i);
+			// Verify that this is a letter (A-Z)
+			if(c<'A' || c>'Z') {
+				System.out.println("ERROR: setPossibleLetters() take a String with letters (A-Z), value provided was: " + c);
+				return;
+			}
+			// Check against duplicates
+			if(p.lastIndexOf(c) != i) {
+				System.out.println("ERROR: setPossibleLetters() requires " + N_POSSIBLE_LETTERS + " unique letters. The letter '" + c + "' appears twice.");
+				return;
+			}
+		}
+		this.possibleLetters = p;
+	}
+	
+	/**
+	 * @return The {@code possibleLetters} field - may be null
+	 */
+	public String getPossibleLetters() {
+		return this.possibleLetters;
+	}
+	
+	/**
+	 * Verifies that {@code possibleLetters} are valid - 6 unique letters (A-Z)
+	 * @return {@code true} if {@code possibleLetters} are valid.
+	 */
+	public boolean hasValidPossibleLetters() {
+		if(this.possibleLetters == null || this.possibleLetters.length() != N_POSSIBLE_LETTERS) {
+			return false;
+		}
+		for(Character c : this.getPossibleLetters().toUpperCase().toCharArray()) {
+			// Verify that this is a letter (A-Z)
+			if(c < 'A' || c > 'Z') {
+				return false;
+			}
+			// Check against duplicates
+			if(this.getPossibleLetters().toUpperCase().lastIndexOf(c) != this.getPossibleLetters().toUpperCase().indexOf(c)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Verifies that there is a valid {@link TreeMapLanguageDictionary} object and that {@code requiredLetter} and {@code possibleLetters} 
+	 * are set an valid. Then applies a filter to create a new {@link TreeMapLanguageDictionary} object with words that contain 
+	 * {@code requiredLetter} and whose only other letters are in {@code possibleLetters}.
+	 * Results are stored in a {@link SpellingBeeSolution} object.
+	 * @return A fully populated {@link SpellingBeeSolution} object.
+	 */
+	public SpellingBeeSolution solve() {
+		SpellingBeeSolution sln = new SpellingBeeSolution();
+		
+		// Check that dictionary, requiredLetter, and possibleLetters are valid
+		if(!this.hasValidDictionary()) {
+			System.out.println("ERROR: No valid dictioanry to solve Spelling Bee with.");
+			return sln;
+		}
+		if(!this.hasValidRequiredLetter()) {
+			System.out.println("ERROR: No valid required letter to solve Spelling Bee with.");
+			return sln;
+		}
+		if(!this.hasValidPossibleLetters()) {
+			System.out.println("ERROR: No valid possible letters to solve Spelling Bee with.");
+			return sln;
+		}
+		
+		// Create a filter based on requiredLetter and possibleLetters and get all valid words from the dictionary
+		DictionaryFilter filter = new DictionaryFilter();
+	    filter.addRequiredLetters(this.getRequiredLetter());
+	    filter.addPossibleLetters(this.getPossibleLetters());
+	    TreeMapLanguageDictionary spellingBeeAnswers = (TreeMapLanguageDictionary)filter.applyTo(this.getDictionary());
+	    
+	    // Populate the SpellingBeeSolution object
+	    sln.setAllAnswers(spellingBeeAnswers);
+	    
+	    //For each word in the full solution set, check if it is a pangram (uses all 6 letters) and also store by word length
+	    for(Word w : spellingBeeAnswers) {
+	    	// Check for pangram
+	    	boolean pangram = true;
+	    	for(char c : this.getPossibleLetters().toCharArray()) {
+	    		if(w.getLetters().indexOf(c) < 0) {
+	    			pangram = false;
+	    			break;
+	    		}
+	    	}
+	    	if(pangram) {
+	    		sln.addPangram(w);
+	    	}
+	    	// Store word by word length
+	    	sln.addAnswerByLength(w);
+	    }
+	    return sln;
+	}
+	
+	/**
+	 * Holds the solution set to a Spelling Bee puzzle. Set of all answers is stored
+	 * as a {@link TreeMapLanguageDictionary} object in {@code allAnsers}. All answers that are pangrams
+	 * (i.e. use all letters in the puzzle) are stored as an {@link ArrayList}&lt;{@link Word}&gt; in 
+	 * {@code pangrams}. Answers are also organized by word length in {@code answersByLength}
+	 * 
+	 */
+	public static class SpellingBeeSolution {
+		private List<Word> pangrams;
+		private Map<Integer, ArrayList<Word>> answersByLength;
+		private TreeMapLanguageDictionary allAnswers;
+		
+		/**
+		 * Initialize an empty SpellingBeeSolution object
+		 */
+		public SpellingBeeSolution() {
+			this.allAnswers = new TreeMapLanguageDictionary();
+			this.pangrams = new ArrayList<>();
+			this.answersByLength = this.getAnswersByLength();
+		}
+		
+		/**
+		 * Assign a {@link TreeMapLanguageDictionary} object as the set of all answers
+		 * @param d {@link TreeMapLanguageDictionary} object with answers
+		 */
+		public void setAllAnswers(TreeMapLanguageDictionary d) {
+			this.allAnswers = d;
+		}
+		
+		/**
+		 * Returns the {@link TreeMapLanguageDictionary} object with all answers
+		 * @return {@link TreeMapLanguageDictionary} object with all answers
+		 */
+		public TreeMapLanguageDictionary getAllAnswers() {
+			if(this.allAnswers == null) {
+				this.allAnswers = new TreeMapLanguageDictionary();
+			}
+			return this.allAnswers;
+		}
+		
+		/**
+		 * Add a {@link Word} to the list of pangrams. Note that there is no check here 
+		 * that the word is included in the {@code allAnswers} dictionary
+		 * @param w {@link Word} to add to the list of pangrams
+		 */
+		public void addPangram(Word w) {
+			this.getPangrams().add(w);
+		}
+		
+		/**
+		 * Return the {@code pangrams} object - initializes the list if it is null
+		 * @return The {@code pangrams} field - {@link ArrayList}&lt;{@link Word}&gt; of solutions that use all letters
+		 */
+		public List<Word> getPangrams() {
+			if(this.pangrams == null) {
+				this.pangrams = new ArrayList<>();
+			}
+			return this.pangrams;
+		}
+		
+		/**
+		 * Takes a {@link Word} object and stores it in {@code answersByLength} based on its length
+		 * Note that there is no check that the word is included in the {@code allAnswers} dictionary
+		 * @param w {@link Word} to store
+		 */
+		public void addAnswerByLength(Word w) {
+			if(w == null || w.toString().isBlank())
+				return;
+			Integer l = w.getLetters().length();
+			if(!this.getAnswersByLength().containsKey(l)) {
+				this.getAnswersByLength().put(l, new ArrayList<>());
+			}
+			this.getAnswersByLength().get(l).add(w);
+		}
+		
+		/**
+		 * Returns a {@link TreeMap}&lt;{@link Integer}, {@link ArrayList}&lt;{@link Word}&gt;&gt; that allows
+		 * solutions to be retrieved based on word length. 
+		 * @return {@link TreeMap} from {@link Integer} to an {@link ArrayList} of all {@link Word} objects with that length that are 
+		 * valid answers
+		 */
+		public Map<Integer, ArrayList<Word>> getAnswersByLength() {
+			if(this.answersByLength == null) {
+				// Store answers by length in decreasing order - i.e.. reverse default sorting for Integers
+				this.answersByLength = new TreeMap<>(new Comparator<Integer>() {
+				    public int compare(Integer x, Integer y) {
+				        return Integer.compare(y, x);
+				    }
+				});
+			}
+			return this.answersByLength;
+		}
+		
+		/**
+		 * Displays all of the solution words in the console - first any Pangrams and then 
+		 * all solution words sorted by word length (largest to smallest), e.g.
+		 * <pre>
+		 * PANGRAM(S):
+		 * 	PHANTOM
+		 * 
+		 * WORDS OF LENGTH 9:
+		 * 	MANHATTAN
+		 * 	OOMPAHPAH
+		 * WORDS OF LENGTH 8:
+		 * 	PHOTOMAP
+		 * WORDS OF LENGTH 7:
+		 * 	MAHATMA
+		 * 	MAMMOTH
+		 * 	OTTOMAN
+		 * 	PHANTOM
+		 * 	POMPANO
+		 * WORDS OF LENGTH 6:
+		 * 	AMMONO
+		 * 	ATAMAN
+		 * 	HAMMAM
+		 * ...
+		 * </pre>
+		 */
+		public void prettyPrint() {
+			System.out.println("PANGRAM(S):");
+			if(this.getPangrams().isEmpty()) {
+				System.out.println("\tNONE");
+			} else {
+				for(Word p : this.getPangrams()) {
+					System.out.println("\t"+p);
+				}
+			}
+			System.out.println("");
+			if(this.getAnswersByLength().isEmpty()) {
+				System.out.println("NO ANSWERS FOUND");
+			}
+			for (Map.Entry<Integer, ArrayList<Word>> entry : this.getAnswersByLength().entrySet()) {
+				System.out.println("WORDS OF LENGTH " + entry.getKey() + ":");
+				for(Word w : entry.getValue()) {
+					System.out.println("\t"+w);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Generate solutions to a NYT Spelling Bee puzzle
+	 * @param args: <br>
+	 *   1: requiredLetter - the single yellow letter that all solutions must use
+	 *   2: possibleLetters - the six white letters that solutions may use
+	 *   3: dictionary to use, default is "./test/dictionaries/NWL2023.txt"
+	 */
+	public static void main(String[] args) {
+		// Default dictionary
+		String dictionarypath = "./test/dictionaries/NWL2023.txt";
+		String requiredLetter = "";
+		String possibleLetters = "";
+		if(args.length < 2) {
+			System.out.println("ERROR: at least two arguments are required: requiredLetter and possibleLetters.");
+			return;
+		}
+		requiredLetter = args[0];
+		possibleLetters = args[1];
+		if(args.length == 3) {
+			dictionarypath = args[2];
+		}
+		if(args.length > 3) {
+			System.out.println("WARNING: Only 3 arguments are accepted - additional arguments are ignored.");
+		}
+		TreeMapLanguageDictionary dictionary = new TreeMapLanguageDictionary();
+		DictionaryLoadUtils.loadFromZyzzyva(dictionary,Path.of(dictionarypath));
+		SpellingBeeSolver slvr = new SpellingBeeSolver();
+		slvr.setDictionary(dictionary);
+		slvr.setPossibleLetters(possibleLetters);
+		slvr.setRequiredLetter(requiredLetter);
+		SpellingBeeSolution sln = slvr.solve();
+		sln.prettyPrint();
+	}
+}
